d5cea871fd63c0b137d64fcaba0f97d26474f977
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-09-24 12:59:48 -0400

    Animation of elephant

--- Elephant.java
+++ Elephant.java
@@ -70,6 +70,19 @@ public class Elephant extends Actor
     
     
     public void act()
-    {    
+    {
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-2);
+            facing = "left";
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(2);
+            facing = "right";
+        }
+        
+        // Animate the elephant
+        animateElephant();
     }
 }
