cc77d3d1cca3e97393442dce312ccc214351ddf1
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-09-24 12:58:31 -0400

    Adding Idle of elephant

---
+++ Elephant.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * 
+ * @author Gloriana Zheng
+ * @version Sep. 2024
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    // Direction the elephant is facing
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    /**
+    * Constructor - The code that gets run one time when object is created
+    */
+    
+    public Elephant()
+    {
+        //Array
+        for(int i = 0; i < 8; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        animationTimer.mark();
+        
+        // Initial elephant image
+        setImage(idleRight[0]);
+    }
+    public void act()
+    {    
+    }
+}
